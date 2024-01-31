68872b797950fb456a6094404a815939279804f9
shykemod <nguyenlenhatvu27@gmail.com>
2024-02-01 00:16:51 +0700

    4

--- Asm/Js/Controller.js
+++ Asm/Js/Controller.js
@@ -48,6 +48,9 @@ myApp.config(function ($routeProvider) {
         }).when('/giohang', {
             templateUrl: 'page/GioHang.html',
             controller: 'GioHangCtrl'
+        }).when("/thanhtoan", {
+            templateUrl: 'page/ThanhToan.html',
+            controller: 'ThanhToanCtrl'
         }).when("/qldanhmuc", {
             templateUrl: 'page/QuanLyDanhMuc.html',
             controller: 'qldanhmucCtrl'
@@ -108,7 +111,7 @@ myApp.controller('DangKyCtrl', function ($scope, $http) {
         id: '',
         password: '',
         role: 0,
-        giahang: []
+        giohang: []
     };
 
     $scope.passwordAcp = '';
@@ -286,7 +289,6 @@ myApp.controller('SanPhamChiTietCtrl', function ($scope, $rootScope, $http, $rou
     $scope.ThemGioHang = function () {
         $http.get('http://localhost:3000/user/' + $rootScope.UserId).then(function (response) {
             $scope.UserData = response.data;
-
             for (var i = 0; i < $scope.UserData.giohang.length; i++) {
                 if ($scope.UserData.giohang[i].idSanPham == Number($scope.id)) {
                     $scope.UserData.giohang[i].SoLuong += $scope.soluong
@@ -309,7 +311,9 @@ myApp.controller('SanPhamChiTietCtrl', function ($scope, $rootScope, $http, $rou
             }).catch(function (error) {
                 alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
             });
-        })
+        }).catch(function (error) {
+            alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
+        });
     }
 
     $scope.increment = function () {
@@ -328,6 +332,7 @@ myApp.controller('GioHangCtrl', function ($scope, $rootScope, $http) {
     $scope.TongTien = 0;
     $http.get('http://localhost:3000/user/' + $rootScope.UserId).then(function (response) {
         $scope.UserData = response.data;
+        console.log($scope.UserData)
         $rootScope.giohang = [];
         $scope.UserData.giohang.forEach(element => {
             $http.get('http://localhost:3000/sanpham/' + element.idSanPham).then(function (response) {
@@ -344,6 +349,7 @@ myApp.controller('GioHangCtrl', function ($scope, $rootScope, $http) {
         })
     }).catch(function (error) {
         alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
+        console.log(error);
     });
 
     $scope.Xoa = function (id) {
@@ -464,18 +470,134 @@ myApp.controller('qldanhmucCtrl', function ($scope, $http, $rootScope) {
         name: "",
         img: ""
     }
-    $http.get("http://localhost:3000/danhmuc").then(function (response) {
-        $scope.danhmuc = response.data;
-    }).catch(function (error) {
-        alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
+    $scope.ObjDanhMuc = {
+        id: "",
+        name: "",
+        img: ""
+    }
+    const suaBackdropModal = new bootstrap.Modal(document.getElementById('SuaBackdrop'), {
+        keyboard: false
     });
-    $http.get("http://localhost:3000/sanpham").then(function (response) {
-        $scope.sanpham = response.data;
-    }).catch(function (error) {
-        alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
-    });
-    
+
+
+    $scope.Load = function () {
+        $http.get("http://localhost:3000/danhmuc").then(function (response) {
+            $scope.danhmuc = response.data;
+        }).catch(function (error) {
+            alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
+        });
+        $http.get("http://localhost:3000/sanpham").then(function (response) {
+            $scope.sanpham = response.data;
+        }).catch(function (error) {
+            alertMessage("Có vẻ như đã xảy ra lỗi", "danger");
+        });
+    }
+
+    $scope.Load();
+
+    $scope.Them = function () {
+        if ($scope.NewDanhMuc.id == "") {
+            alert("Vui lòng nhập Đường dẫn");
+            return;
+        }
+        if ($scope.NewDanhMuc.name == "") {
+            alert("Vui lòng nhập Tên danh mục");
+            return;
+        }
+        var fileInput = document.getElementById('AnhDanhMuc');
+        var file = fileInput.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $scope.$apply(function () {
+                    $scope.NewDanhMuc.img = e.target.result;
+                    $http.post('http://localhost:3000/danhmuc', $scope.NewDanhMuc).then(function (response) {
+                        $scope.NewDanhMuc = {
+                            id: "",
+                            name: "",
+                            img: ""
+                        }
+                        fileInput.value = "";
+                        $scope.Load();
+                        alert("Thêm thành công");
+                    }).catch(function (error) {
+                        alert("Thêm thất bại, Đường dẫn đã tồn tại");
+                    });
+                });
+            };
+            reader.readAsDataURL(file);
+        } else {
+            alert("Vui lòng nhập ảnh");
+            return;
+        }
+    };
+
+    $scope.ViewSua = function (item) {
+        $scope.ObjDanhMuc.id = item.id;
+        $scope.ObjDanhMuc.name = item.name;
+        $scope.ObjDanhMuc.img = item.img;
+    }
+
+    $scope.Sua = function () {
+        if ($scope.ObjDanhMuc.id == "") {
+            alert("Vui lòng nhập Đường dẫn");
+            return;
+        }
+        if ($scope.ObjDanhMuc.name == "") {
+            alert("Vui lòng nhập Tên danh mục");
+            return;
+        }
+
+        var fileInput = document.getElementById('AnhDanhMuc2');
+        var file = fileInput.files[0];
+
+        var updateDanhMuc = function () {
+            $http.put('http://localhost:3000/danhmuc/' + $scope.ObjDanhMuc.id, $scope.ObjDanhMuc).then(function (response) {
+                $scope.ObjDanhMuc = {
+                    id: "",
+                    name: "",
+                    img: ""
+                };
+                fileInput.value = "";
+                $scope.Load();
+                alert("Sửa thành công");
+                suaBackdropModal.hide();
+            }).catch(function (error) {
+                alert("Sửa thất bại, Đường dẫn đã tồn tại");
+            });
+        };
+
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $scope.$apply(function () {
+                    $scope.ObjDanhMuc.img = e.target.result;
+                    updateDanhMuc();
+                });
+            };
+            reader.readAsDataURL(file);
+        } else {
+            updateDanhMuc();
+        }
+    };
+
+
+    $scope.Xoa = function (id) {
+        if (confirm("Bạn có chắc chắn muốn xóa") == true) {
+            $http.delete("http://localhost:3000/danhmuc/" + id).then(function (response) {
+                $scope.Load();
+                alert("Xóa thành công");
+            }).catch(function (error) {
+                alert("Có vẻ như đã xảy ra lỗi");
+            });
+        }
+    };
+
     $scope.getSoLuong = function (id) {
-        return $scope.sanpham.filter((sp) => sp.danhmuc == id).length;
+        try {
+            return $scope.sanpham.filter((sp) => sp.danhmuc == id).length;
+        } catch (error) {
+            return 0;
+        }
     };
 });
